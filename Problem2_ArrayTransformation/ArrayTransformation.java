9dcaf4c43005653c344366f47e1ee5633e7c2a7d
Sanjana Gupta <guptaraani627@gmail.com>
2026-05-05 13:50:28 +0530

    Implement minOperations method for array transformation

--- Problem2_ArrayTransformation/ArrayTransformation.java
+++ Problem2_ArrayTransformation/ArrayTransformation.java
@@ -1,1 +1,36 @@
+import java.util.*;
 
+public class ArrayTransformation {
+
+    public static int minOperations(int[] arr, int k) {
+        Arrays.sort(arr);
+        int target = arr[arr.length / 2];
+
+        int operations = 0;
+
+        for (int num : arr) {
+            int diff = Math.abs(num - target);
+
+            if (diff % k != 0) return -1;
+
+            operations += diff / k;
+        }
+
+        return operations;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int k = sc.nextInt();
+
+        System.out.println(minOperations(arr, k));
+    }
+}
